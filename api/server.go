85e67418406b7ac66b9909ec17bce0e7b0569e3a
Bill Kerr <48515765+bk7987@users.noreply.github.com>
2020-09-25 08:08:23 -0400

    Added cors middleware

--- api/server.go
+++ api/server.go
@@ -11,6 +11,7 @@ import (
 	"github.com/bk7987/timecards/jobs"
 	"github.com/bk7987/timecards/timecards"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/gofiber/fiber/v2/middleware/recover"
 	"gorm.io/gorm"
 )
@@ -41,8 +42,9 @@ func main() {
 
 	app := fiber.New()
 	app.Use(recover.New())
-	api := app.Group("/api")
+	app.Use(cors.New())
 
+	api := app.Group("/api")
 	v1 := api.Group("/v1", config.SetConfig(), heavyjob.SetClient())
 
 	v1.Get("/jobs", jobs.GetJobs)
